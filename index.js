c4ceecd5a03a8cabf297a52d5a11673601da09e5
WebCoder <125247868+WebCoderLider@users.noreply.github.com>
2023-05-10 12:02:47 +0500

    end
    o'rnatilishi kere bo'lgan narsala package.json da bor

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const { v4: uuidv4 } = require('uuid');
+const cors = require('cors')
+const { read_file, write_file } = require('./fs/fs')
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const products = read_file('products')
+
+app.get('/', (req, res) => {
+    res.json(products)
+})
+app.post('/', (req, res) => {
+    const { phone } = req.body
+    if (phone == '' ) {
+        return res.send('error')
+    }
+    else {
+        const product = {
+            id: uuidv4(),
+            phone
+        }
+        products.push(product)
+        write_file('products', products)
+        res.json(product)
+    }
+})
+app.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const index = products.findIndex((product) => product.id === id);
+
+    if (index !== -1) {
+        const deletedProduct = products.splice(index, 1);
+        write_file('products', products);
+        res.json(deletedProduct);
+    } else {
+        res.status(404).send("Product not found");
+    }
+});
+
+
+app.listen(5000, console.log('server 8080 port da ishga tushdi'))
